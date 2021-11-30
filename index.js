bae8bed3bd37790d29ac42c95722b18dda2f93e5
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-11-30 17:11:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/*const alertEx = document.querySelector('.btn')
+alertEx.addEventListener('click', function(event) {
+    alert(`You have clicked the ${event.target.innerText} number`);
+});
+*/
+
+/*
+const display = document.querySelector('.display-container');
+buttonClicked.addEventListener('click', function() {
+    display.innerText = input.value
+    });
+*/
+/* FX
+const isDisplayed = document.querySelector()
+
+const addOp() => {};
+
+const substractOp() => {};
+
+const multiplyOp() => {};
+
+const divideOp() => {};
+*/
